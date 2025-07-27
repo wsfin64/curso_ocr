9791d1145156cb7291e2fd201e0078580f6245f2
Wellington Silva <wsfin64@gmail.com>
2025-07-27 12:34:14 -0300

    reconhecendo idioma português

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ import cv2
 
 img = cv2.imread('./imagens/content/teste01.jpg')
 
-rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+# É realmente necessário converter a imagem para RGB? Aparentemente não, mas é uma boa prática para garantir compatibilidade com diferentes formatos de imagem.
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
 
 texto: str = pytesseract.image_to_string(rgb)
+print(texto)
+
+# Utilizando o idioma português
+img = cv2.imread('./imagens/content/teste02.jpg')
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+texto = pytesseract.image_to_string(rgb, lang='por')
 print(texto)
